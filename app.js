a3102032a121c60900404af4410076a2afd79f33
David <sandavid08@yahoo.com>
2024-09-04 11:30:51 -0700

    fixed #3

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(
-  expressSession({
+  session({
     cookie: {
      maxAge: 7 * 24 * 60 * 60 * 1000 // ms
     },
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 const passport = require("../authenticate/passport")
+const multer = require('multer')
+const upload = multer({ dest: 'uploads/'})
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { user: req.user, title: 'Express' });
@@ -20,4 +22,12 @@ router.get("/log-out", (req, res, next) => {
     res.redirect("/");
   });
 });
+
+router.get('/upload', function(req, res, next) {
+    res.render('upload')
+})
+router.post('/upload', upload.single('uploaded_file'), function(req, res, next) {
+  console.log(req.file, req.body)
+})
+
 module.exports = router;
